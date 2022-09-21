c3c9aa09b1889c12c72efe1a7fd5c1f3b06e9eb9
yem1546 <87500366+yem1546@users.noreply.github.com>
2022-09-21 02:01:31 -0400

    added styled components

--- src/components/Body.js
+++ src/components/Body.js
@@ -2,6 +2,33 @@ import { React, useState, useEffect } from "react";
 import Image from "./Image";
 import API from "../constants";
 import axios from "axios"
+import styled from 'styled-components'
+
+const StyledHeader = styled.div`
+  margin: 6px;
+  padding: 10px;
+  background-color: #aeb0b5;
+  input, button {
+    margin: 5px;
+    padding: 5px;
+    border: none;
+    border-radius: 3px;
+  }
+`
+const StyledBody = styled.div`
+  background-color: #00a6d2;
+  h1 {
+    color: #5b616b;
+  }
+  p {
+    margin: 10%, 0;
+    padding: 5%;
+    background-color: #e1f3f8;
+    border: none;
+    border-radius: 10px;
+  }
+`
+
 
 const Body = (props) => {
 
@@ -39,11 +66,11 @@ const Body = (props) => {
 
   console.log("active", activeDate)
   return (
-    <div className="body">
-      <div>
+    <StyledBody className="body">
+      <StyledHeader>
         <input type={"date"} onChange={changeDate} defaultValue={activeDate} max={props.today}></input>
         <button onClick={setDate}>Get Image</button>
-      </div>
+      </StyledHeader>
         <h1>
           {imageData && imageData.title}
         </h1>
@@ -51,7 +78,7 @@ const Body = (props) => {
         <p>
           {imageData && imageData.explanation}
         </p>
-      </div>
+    </StyledBody>
   )
 }
 
